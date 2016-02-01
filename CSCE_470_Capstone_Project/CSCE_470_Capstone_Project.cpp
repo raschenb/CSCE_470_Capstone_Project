fc2181e395acf1ba2276f6b880407db569f0a5bb
Robert Aschenbrenner <raschenb@alaska.edu>
2016-02-01 13:15:18 -0900

    CSCE_470_Capstone_Software_Holder(Blank w/Licence)
    This was needed to show we had a Git for our Capstone Project. Blank
    Software with BSD-2 licence as required.

---
+++ CSCE_470_Capstone_Project/CSCE_470_Capstone_Project.cpp
@@ -0,0 +1,48 @@
+// CSCE_470_Capstone_Project.cpp : Defines the entry point for the console application.
+//
+// NOTE: This software is a place holder for my CSCE 470 Computer Science 
+// Capstone Project. Our first project besides the beginning of the 
+// Thesis was to create a Git with some placer to show the repository was 
+// done and show what license. (BSD-2) 
+//
+//Copyright(c) <2016>, <Robert Aschenbrenner>
+//All rights reserved.
+//
+//Redistribution and use in source and binary forms, with or without
+//modification, are permitted provided that the following conditions are met :
+//
+//1. Redistributions of source code must retain the above copyright notice, this
+//list of conditions and the following disclaimer.
+//2. Redistributions in binary form must reproduce the above copyright notice,
+//this list of conditions and the following disclaimer in the documentation
+//and / or other materials provided with the distribution.
+//
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//DISCLAIMED.IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+//ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//The views and conclusions contained in the software and documentation are those
+//of the authors and should not be interpreted as representing official policies,
+//either expressed or implied, of the FreeBSD Project.
+
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+	cout << "This is my tester software and place holder on Git.\n" << endl;
+	system ("PAUSE");
+    return 0;
+}
+
